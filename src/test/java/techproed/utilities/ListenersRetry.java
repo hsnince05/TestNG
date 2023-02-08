38d62e35efd45976643b3f8477b257dc742e5270
Hasan INCE <hsnc0505@gmail.com>
2023-02-08 16:38:08 +0300

    05/02/2022

--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -3,7 +3,11 @@ package techproed.utilities;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 public class ListenersRetry implements IRetryAnalyzer {
+
+    //    BU SINIF SADECE FAIL OLAN TEST CASELERE TEKRAR CALISTIRIR
     private int retryCount = 0;
+
+    // maxRetryCount EK OLARAK CALISMA SAYISI. BU ORNEKTE FAIL OLAN TESTLER EXTRA BIR DEFA DAHA CALISIR
     private static final int maxRetryCount = 2;
     @Override
     public boolean retry(ITestResult result) {
